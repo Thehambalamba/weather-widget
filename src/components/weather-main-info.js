4fc5d6618740be34efb65d697a0924d0dc3a9cd6
Nikola Pervic <nikolapervic@me.com>
2018-08-25 23:42:24 +0200

    Initial commit

---
+++ src/components/weather-main-info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class WeatherMainInfo extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+       <section className="container">
+        <div className=".info-and-icon">
+          div
+        </div>
+       </section>
+      </React.Fragment>
+    );
+  }
+}
+
+export default WeatherMainInfo;
